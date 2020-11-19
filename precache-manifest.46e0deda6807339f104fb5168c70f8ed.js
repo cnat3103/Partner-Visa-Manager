dd8b6c082651723e6990061d4558cce4c00e0fad
Christopher Natale <christophernatale@ChristophersAir.lan>
2020-11-19 20:39:35 +1100

    Updates

---
+++ precache-manifest.46e0deda6807339f104fb5168c70f8ed.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e11cf5db7d7735a2acec446c7fa895a6",
+    "url": "/Partner-Visa-Mangager/index.html"
+  },
+  {
+    "revision": "dad58f030e87633985e3",
+    "url": "/Partner-Visa-Mangager/static/css/2.58545ea1.chunk.css"
+  },
+  {
+    "revision": "03d23e79957fcfc6f2dd",
+    "url": "/Partner-Visa-Mangager/static/css/main.48d2f796.chunk.css"
+  },
+  {
+    "revision": "dad58f030e87633985e3",
+    "url": "/Partner-Visa-Mangager/static/js/2.59fdccd4.chunk.js"
+  },
+  {
+    "revision": "29eb69a08ea4198f7b46e1db8a3d5045",
+    "url": "/Partner-Visa-Mangager/static/js/2.59fdccd4.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "03d23e79957fcfc6f2dd",
+    "url": "/Partner-Visa-Mangager/static/js/main.3624bca7.chunk.js"
+  },
+  {
+    "revision": "3e169fa4e13c8cc4b9e0",
+    "url": "/Partner-Visa-Mangager/static/js/runtime-main.d0cde485.js"
+  }
+]);
